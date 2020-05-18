49ec5beb056b73dcf624ecf047d08d2a2f3c7fce
Devin Strickland <dvnstrcklnd@users.noreply.github.com>
2020-05-18 08:12:26 -0700

    Aquadoc initial commit

---
+++ js/definitions.js
@@ -0,0 +1,83 @@
+var config = {
+
+  tagline: "The Laboratory</br>Operating System",
+  documentation_url: "http://localhost:4000/aquarium",
+  title: "Aquarium RNA Extraction Kits",
+  navigation: [
+
+    {
+      category: "Overview",
+      contents: [
+        { name: "Introduction", type: "local-md", path: "README.md" },
+        { name: "About this Workflow", type: "local-md", path: "ABOUT.md" },
+        { name: "License", type: "local-md", path: "LICENSE.md" },
+        { name: "Issues", type: "external-link", path: 'https://github.com/klavinslab/aq-rna-extraction-kits/issues' }
+      ]
+    },
+
+    
+
+      {
+
+        category: "Operation Types",
+
+        contents: [
+
+          
+
+        ]
+
+      },
+
+    
+
+    
+
+      {
+
+        category: "Libraries",
+
+        contents: [
+
+          
+            {
+              name: 'QIAampDSPViralRNAMiniKit',
+              path: 'libraries/QIAampDSPViralRNAMiniKit' + '.html',
+              type: "local-webpage"
+            },
+          
+            {
+              name: 'RNAExtractionKits',
+              path: 'libraries/RNAExtractionKits' + '.html',
+              type: "local-webpage"
+            },
+          
+            {
+              name: 'TestRNAExtractionKit',
+              path: 'libraries/TestRNAExtractionKit' + '.html',
+              type: "local-webpage"
+            },
+          
+
+        ]
+
+    },
+
+    
+
+    
+      { category: "Sample Types",
+        contents: [
+          
+        ]
+      },
+      { category: "Containers",
+        contents: [
+          
+        ]
+      }
+    
+
+  ]
+
+};
